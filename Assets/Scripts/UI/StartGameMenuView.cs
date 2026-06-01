60aa65a1bc992b642134cb85b20a5aec8d9b2ce1
inviqDev <inviq.dev@gmail.com>
2026-06-01 16:02:31 +0300

    inviqdev 1. Added Start/Exit game menu at the start of the game. The start game flow now looks: Start game => Start/Exit menu => Opponent selection menu => ... 2. Minor changes in the menuViews animation timings => now menus changes faster. It looks not annoying like it was before

---
+++ Assets/Scripts/UI/StartGameMenuView.cs
@@ -0,0 +1,110 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class StartGameMenuView : MenuViewBase
+{
+    [Header("Menu Buttons")]
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private Button exitGameButton;
+
+    public event Action StartGameClicked;
+    public event Action ExitGameClicked;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        ValidateReferences();
+    }
+
+    private void OnEnable()
+    {
+        AddButtonListeners();
+        SetInteractable(true);
+    }
+
+    private void OnDisable()
+    {
+        RemoveButtonListeners();
+        SetInteractable(false);
+    }
+
+    private void OnValidate()
+    {
+        ValidateReferences();
+    }
+
+    protected override void HandleBeforeShow()
+    {
+        SetInteractable(true);
+    }
+
+    protected override void HandleBeforeHide()
+    {
+        SetInteractable(false);
+    }
+
+    private void AddButtonListeners()
+    {
+        if (startGameButton)
+        {
+            startGameButton.onClick.RemoveListener(HandleStartGameClicked);
+            startGameButton.onClick.AddListener(HandleStartGameClicked);
+        }
+
+        if (exitGameButton)
+        {
+            exitGameButton.onClick.RemoveListener(HandleExitGameClicked);
+            exitGameButton.onClick.AddListener(HandleExitGameClicked);
+        }
+    }
+
+    private void RemoveButtonListeners()
+    {
+        if (startGameButton)
+        {
+            startGameButton.onClick.RemoveListener(HandleStartGameClicked);
+        }
+
+        if (exitGameButton)
+        {
+            exitGameButton.onClick.RemoveListener(HandleExitGameClicked);
+        }
+    }
+
+    private void SetInteractable(bool interactable)
+    {
+        if (startGameButton)
+        {
+            startGameButton.interactable = interactable;
+        }
+
+        if (exitGameButton)
+        {
+            exitGameButton.interactable = interactable;
+        }
+    }
+
+    private void HandleStartGameClicked()
+    {
+        StartGameClicked?.Invoke();
+    }
+
+    private void HandleExitGameClicked()
+    {
+        ExitGameClicked?.Invoke();
+    }
+
+    private void ValidateReferences()
+    {
+        if (!startGameButton)
+        {
+            Debug.LogError($"{nameof(StartGameMenuView)} requires a start game button reference.", this);
+        }
+
+        if (!exitGameButton)
+        {
+            Debug.LogError($"{nameof(StartGameMenuView)} requires an exit game button reference.", this);
+        }
+    }
+}
